a4eaec166d48e902cb3828361323f47440e52a83
attanasiomarika <121249639+attanasiomarika@users.noreply.github.com>
2023-04-03 19:41:36 +0200

    Add files via upload

---
+++ MaximumPowerControl/Server.py
@@ -0,0 +1,32 @@
+import cherrypy
+import connect_db as cdb
+import APIs
+
+
+class Server():
+    exposed = True
+    connessione=cdb.Connect("Data_DB/data.db")
+
+    def GET(self, *uri):        
+        apiConn=APIs.api_server(self.connessione)
+        result= apiConn.handleRequestGET(uri[0],uri[1])
+        return result
+        
+
+    def PATCH(self, *uri):
+       apiConn=APIs.api_server(self.connessione)
+       result=apiConn.handlePatchRequest(uri[0], uri[1])
+       return result
+       
+if __name__ == "__main__":
+    # Standard configuration to serve the url "localhost:8080"
+    conf = {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tool.session.on': True
+        }
+    }
+    webService = Server()
+    cherrypy.tree.mount(webService, '/', conf)
+    cherrypy.engine.start()
+    cherrypy.engine.block()
---
+++ MaximumPowerControl/main.py
@@ -0,0 +1,17 @@
+from MaxPowerControl import MaxPowerControl
+
+
+MPC= MaxPowerControl()
+#Carico tutte le case
+# prova=MPC.loadLastPowerDataHouse("H1")
+# prova1=MPC.computeTotalPower("H1")
+# prova2=MPC.loadHousePowerLimit("H1")
+# prova3=MPC.controlLastUpdateModule("H1")
+# prova4=MPC.controlPower("H1")
+prova5=MPC.updateModule("H1")
+
+
+
+
+
+ciao=1
