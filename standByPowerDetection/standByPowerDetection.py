5df2ece4169ce6dbd66cf12622556ea676bd9531
miri <miripassarotto@gmail.com>
2023-05-18 23:44:58 +0200

    works with new db

--- standByPowerDetection/standByPowerDetection.py
+++ standByPowerDetection/standByPowerDetection.py
@@ -1,51 +1,64 @@
+#se il modulo è spento non controllo
+#formato stampa messsaggi ed errori?
+
 import os
 import time
 import sys
 
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-sys.path.append(PROJECT_ROOT)
-from microserviceBase.serviceBase import *
+#PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+#sys.path.append(PROJECT_ROOT)
+#from microserviceBase.serviceBase import *
 import sqlite3
 
 
 class StandByPowerDetection():
 
-    def __init__(self,database):
-        self.conn = sqlite3.connect('C:/Users/hp/Desktop/IOT/lab4_es4/data_m.sqlite')
+    def __init__(self):
+        self.conn = sqlite3.connect('C:/Users/hp/Desktop/IOT/lab4_es4/dbRC.sqlite')
+        self.connHA = sqlite3.connect('C:/Users/hp/Desktop/IOT/lab4_es4/testDB_Marika.db')
         self.cur = self.conn.cursor()
-        self.database= 'data'
-        self.modules_and_switches='Devices'
-        self.ranges='AppliancesInfo'
-        self.devices_db= 'Appliances'
-        self.houses='HouseDev_conn'
+        self.curHA= self.connHA.cursor()
+        self.database= 'states1'
+        self.onlineDev='Devices'  # online
+        self.ranges='AppliancesInfo'  #ok
+        self.devices_settings= 'DeviceSettings' #deviceID,enabledSockets,parContorl 
+        self.housesdev='HouseDev_conn' #Device per house
+        self.houses= 'Houses' #ID
+        #self.client= ServiceBase("C:/Usersokhp/Desktop/IOT/lab4_es4/standByPowerDetection/serviceConfig_standBy.json")
+        #self.client.start()
 
+    
     
     def prevValuesCheck(self, moduleID):
         # Retrieve the 10 largest values of the timestamp column for the given moduleID
-        control.cur.execute("""
-             SELECT deviceID, power FROM (
-                 SELECT deviceID, power, ROW_NUMBER() 
-                 OVER (ORDER BY timestamp DESC) AS row_num
+        partial=moduleID[1:]
+        powerStateID= 'sensor.power_' + partial
+        self.curHA.execute("""
+             SELECT entity_id, state FROM (
+                 SELECT entity_id, state, ROW_NUMBER() 
+                 OVER (ORDER BY last_updated_ts DESC) AS row_num
                  FROM {}
-                 WHERE deviceID = ?
+                 WHERE entity_id = ?
              )
-             WHERE row_num <= 10 """.format(self.database), (moduleID,))
-        results = self.cur.fetchall()
+             WHERE row_num <= 10 """.format(self.database), (powerStateID,))
+        results = self.curHA.fetchall()
+        
         #for result in results:
         #    print(f"ID: {result[0]}, timestamp: {result[1]}")
         return results
 
        
     def lastValueCheck(self, ID):
+        partial=ID[1:]
+        powerStateID= 'sensor.power_' + partial
         #return [(ID, max_timestamp, power)]
         #  retrieve the maximum value of timestamp column for each ID
-        self.cur.execute("""
-            SELECT deviceID, MAX(timestamp), power
+        self.curHA.execute("""
+            SELECT entity_id, MAX(last_updated_ts), state
             FROM {}
-            WHERE deviceID
-            = ?""".format(self.database),(ID,))
-        results = self.cur.fetchone()
-        
+            WHERE entity_id
+            = ?""".format(self.database),(powerStateID,))
+        results = self.curHA.fetchone()
         return results
     
         
@@ -68,25 +81,32 @@ class StandByPowerDetection():
         to_consider=[]
         self.cur.execute("""SELECT deviceID
                          FROM {} 
-                         WHERE houseID = ?""".format(self.houses),(house_ID,))
+                         WHERE houseID = ?""".format(self.housesdev),(house_ID,))
         house_modules= self.cur.fetchall()
         for house_module in house_modules :
-            self.cur.execute("""SELECT *
+            self.cur.execute("""SELECT deviceID, online
                              FROM {}
-                             WHERE deviceID = ? """.format(self.modules_and_switches),(house_module))
+                             WHERE deviceID = ? """.format(self.onlineDev),(house_module))
             result = self.cur.fetchall()
-            #if ( result[0][1] == 1 and sum(result[0][2:])==1):
-            if (result[0][4]==1 and sum([int(x) for x in eval(result[0][5])])==1): 
-                to_consider.append(result)
+            if (result[0][1]==1) : 
+                self.cur.execute("""SELECT deviceID, enabledSockets, parControl
+                                 FROM {}
+                                 WHERE deviceID = ? """.format(self.devices_settings),(house_module))
+                settings = self.cur.fetchall()
+                if  (sum([int(x) for x in eval(settings[0][1])])>1):
+                    print('ERRORE')
+                    break
+                if(settings[0][2] == 1) :
+                    to_consider.append(result)
         if (to_consider) is not None:
             return (to_consider)
-        else: return None    
+        else: return None   
         
     def getRange(self, ID):   
            self.cur.execute("""SELECT applianceType 
                             FROM {} 
                             WHERE deviceID
-                            = ?""".format(self.devices_db), (ID,))
+                            = ?""".format(self.devices_settings), (ID,))
            device_type = self.cur.fetchone()[0]
            self.cur.execute("""SELECT standByPowerMax
                              FROM {} 
@@ -100,28 +120,42 @@ class StandByPowerDetection():
             
     
     def MQTTStandByPowerInterface(self, houseID, ID):
-        client= ServiceBase("C:/Users/hp/Desktop/IOT/lab4_es4/standbypower_control/serviceConfig_example.json")
-
+       #client= ServiceBase("C:/Users/hp/Desktop/IOT/lab4_es4/standByPowerDetection/serviceConfig_standBy.json")
+        #client.start()
         self.cur.execute("""SELECT switchesStates
                             FROM {}
                             WHERE deviceID = ? """.format(self.modules_and_switches),(ID,))
-        switches = self.cur.fetchone()[0]
-        topic="SmartModule/{}/dev/{}".format(houseID, ID)
+        #switches = self.cur.fetchone()[0]
+        switches = ['1','0','1']
+        #topic="SmartModule/{}/dev/{}".format(houseID, ID)
+        topic= "prova/prova/msmartmod"
         msg=  {
             "StandBy power consumption":{  
             "Active": {
-            "Module": 0, 
+            "Module": 0, #id
             "Switches": switches
         }}}
         
+        self.client.MQTT.start()
+        #client.MQTT.Subscribe(topic)
+        self.client.MQTT.Publish(topic, msg)
+        self.client.MQTT.stop() 
+
+    '''def mqttprova(self):
+        client= ServiceBase("C:/Users/hp/Desktop/IOT/lab4_es4/standByPowerDetection/serviceConfig_standBy.json")
+        client.start()
+        topic= "prova/prova/msmartmod"
+        msg=  {
+            "StandBy power consumption provaproava"}
         client.MQTT.start()
-        client.MQTT.Subscribe(topic)
         client.MQTT.Publish(topic, msg)
-        client.MQTT.stop() 
+
+        client.MQTT.stop() '''
+
 
     def controlAndDisconnect(self):
-        self.cur.execute("""SELECT DISTINCT houseID
-                            FROM {}""".format( self.houses))
+        self.cur.execute("""SELECT houseID
+                    FROM {}""".format( self.houses))
         houses = self.cur.fetchall()
         for house in houses:
             modules=  self.houseInfo(house[0])
@@ -129,20 +163,21 @@ class StandByPowerDetection():
                 standByPowercont=0 
                 value= self.getRange(info[0][0]) #info[i][0] = ID
                 last_measurement= self.lastValueCheck(info[0][0])#[id, time,power]
-                if (last_measurement[2]>=1 or last_measurement[2]<=value):
+                if (int(last_measurement[2])>=1 or int(last_measurement[2])<=value):
                     prevRows= self.prevValuesCheck(info[0][0])
                     for prevValues in prevRows:
-                        if (prevValues[1]>=1 or prevValues[1]<=value):
+                        if (int(prevValues[1])>=1 or int(prevValues[1])<=value):
                             standByPowercont+=1   
                         if standByPowercont>=4:
-                            self.MQTTStandByPowerInterface(house[0], info[0][0])
+                            #self.MQTTStandByPowerInterface(house[0], info[0][0])
+                            print('off', info[0][0])
                             break
 
 
 if __name__ == "__main__":
-    control= StandByPowerDetection('C:/Users/hp/Desktop/IOT/lab4_es4/data_m.sqlite')
+    control= StandByPowerDetection()
     i=0
-    while(i>3): 
+    while(i<3): 
         control.controlAndDisconnect()
         time.sleep(2)
-       
+        i+=1
