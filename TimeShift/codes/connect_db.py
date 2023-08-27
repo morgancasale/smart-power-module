62a2197d209ac4d61cf56f7a50d22c8dcf2cebe9
attanasiomarika <121249639+attanasiomarika@users.noreply.github.com>
2023-08-27 11:08:19 +0200

    Add files via upload

---
+++ TimeShift/codes/connect_db.py
@@ -0,0 +1,16 @@
+import sqlite3 
+from sqlite3 import Error
+
+class Connect():
+    def __init__(self,DBPath):
+        self.DBPath = DBPath
+    
+    def create_connection(self):
+        conn = None
+    
+        try:
+            conn = sqlite3.connect(self.DBPath)
+        except Error as err:
+            print(err)
+
+        return conn
