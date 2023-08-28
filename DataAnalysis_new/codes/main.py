8bd84e8a2ba8a69e2a42b22ec1c1abd90cc8faaf
attanasiomarika <121249639+attanasiomarika@users.noreply.github.com>
2023-08-28 17:21:15 +0200

    Add files via upload

--- DataAnalysis_new/codes/main.py
+++ DataAnalysis_new/codes/main.py
@@ -1,35 +1,34 @@
-from data_analysis import DataAnalysis
+from data_analysis2 import DataAnalysis
 DA= DataAnalysis()
-import time
+
 
 # prova=DA.getalldata() 
-#DA.compute_hourlyavgData()
-#prova2=DA.compute_hourlytotData()
+prova1a=DA.compute_hourlyavgdata()
+prova1b=DA.compute_hourlytotdata()
 
 # prova2=DA.get_hourlydata()
-# prova2a=DA.compute_dailyavgData()
-# prova2b=DA.compute_dailytotData()
+# prova2a=DA.compute_dailyavgdata()
+# prova2b=DA.compute_dailytotdata()
+
 
 # prova3=DA.get_dailydata()
-# prova3a=DA.compute_monthlyavgData()
-# prova3b=DA.compute_monthlytotData()
+# prova3a=DA.compute_monthlyavgdata()
+# prova3b=DA.compute_monthlytotdata()
 
 # prova4=DA.get_monthlydata()
-# prova4a=DA.compute_yearlyavgData()
-# prova4b=DA.compute_yearlytotData()
+# prova4a=DA.compute_yearlyavgdata()
+# prova4b=DA.compute_yearlytotdata()
 
 
-#prova5=DA.compute_hourlyDataHouse('H1')
-#prova5a=DA.findmaxinaday('H1')
+# prova5=DA.compute_hourlyDataHouse('H1')
 
 
-#prova6=DA.compute_dailyDataHouse('H1')
-# prova6a=DA.findmaxinamonth('H1')
+# prova6=DA.compute_dailyDataHouse('H1')
 
-#prova7=DA.compute_monthlyDataHouse('H1')
-# prova7a=DA.findmaxinayear('H1')
+# prova7=DA.compute_monthlyDataHouse('H1')
 
-#prova8=DA.compute_yearlyDataHouse('H1')
-prova9=DA.process_data()
+
+# prova8=DA.compute_yearlyDataHouse('H1')
+#prova9=DA.process_data()
 
 ciao=1
