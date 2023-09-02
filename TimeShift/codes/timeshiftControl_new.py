042de6f2d0bf13622f6d4102c15f71f118d6242d
attanasiomarika <121249639+attanasiomarika@users.noreply.github.com>
2023-09-02 20:51:41 +0200

    Add files via upload

--- TimeShift/codes/timeshiftControl_new.py
+++ TimeShift/codes/timeshiftControl_new.py
@@ -43,7 +43,19 @@ class TimeShift():
                 }        
         str_msg = json.dumps(msg, indent=2)
         self.client.MQTT.Publish(topic, str_msg)
-        
+
+    
+    def update_remove_info(self,repeat,deviceID):
+        repeat-=1
+        if repeat>0:
+            query="UPDATE DeviceScheduling SET repeat = ? WHERE deviceID = ?"
+            self.curs.execute(query,(repeat,deviceID))
+            self.conn.commit()
+        elif repeat==0:
+            query="DELETE FROM DeviceScheduling WHERE deviceID = ?"
+            self.curs.execute(query, (deviceID,))
+            self.conn.commit()
+            
 
     def manageService(self, houseID):
         current_time = time.time()
@@ -54,19 +66,34 @@ class TimeShift():
             end_time = row['endTime'] if row['enableEnd'] else None
             deviceID = row['deviceID']
             mode = row['mode']
+            repeat=row['repeat']
+            time_diff = current_time - start_time
+            days_passed = time_diff / (24 * 60 * 60)  
+            
+            if days_passed >= 1:
+                days_to_add = int(days_passed)
+                start_time += days_to_add * (24 * 60 * 60)  
+                if end_time is not None:
+                    end_time += days_to_add * (24 * 60 * 60)  
+        
             if current_time == start_time:
                 if mode == 'ON':
                     self.MQTTInterface(deviceID,'ON')
+                    if end_time is None:
+                        self.update_remove_info(repeat,deviceID)
                 else:
                     self.MQTTInterface(deviceID,'OFF')
+                    if end_time is None:
+                        self.update_remove_info(repeat,deviceID)
 
             if end_time is not None and current_time == end_time:
                 if mode == 'ON':
                     self.MQTTInterface(deviceID,'OFF')
+                    self.update_remove_info(repeat,deviceID)
                 else:
                     self.MQTTInterface(deviceID,'ON')
-            
-
+                    self.update_remove_info(repeat,deviceID)
+                    
     def manageServiceforall(self):
         query="SELECT houseID\
                 FROM Houses"
