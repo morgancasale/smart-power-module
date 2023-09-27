6e2bd0b1819406e3ecfd1806064703aaa2f4bdf9
Morgan Casale <morgan.casale@gmail.com>
2023-09-27 12:53:46 +0200

    Modified afterHA to turn on local_ip integration

--- after_HA.py
+++ after_HA.py
@@ -1,6 +1,10 @@
 import webbrowser
 import requests
 
+"""
+Run this script on the host computer after Home Assistant is up and running.
+"""
+
 try:
     # Open the web browser to the Home Assistant page where to configure the MQTT integration
     url = "http://127.0.0.1:8123/_my_redirect/config_flow_start?domain=mqtt"
@@ -25,5 +29,11 @@ try:
     print("Broker port: " + str(brokerInfo["port"]))
     print("Broker username: " + brokerInfo["MQTTUser"])
     print("Broker password: " + brokerInfo["MQTTPassword"])
+    print("")
+
+    input("Press Enter to continue...")
+
+    # Open the web browser to the Home Assistant page where to configure the localIP integration
+    url = "http://127.0.0.1:8123/_my_redirect/config_flow_start?domain=local_ip"
 except Exception as e:
     print(str(e))
