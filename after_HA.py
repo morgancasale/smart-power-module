d3dfbdfa6c4dd471c20d0d7ab310204ba3874737
Morgan Casale <morgan.casale@gmail.com>
2023-11-01 16:58:55 +0100

    Solved bug in after_HA while running on raspberry pi

--- after_HA.py
+++ after_HA.py
@@ -1,14 +1,24 @@
 import webbrowser
 import requests
+import subprocess
 
 """
 Run this script on the host computer after Home Assistant is up and running.
 """
 
+def open_web_page(url):
+    ps = subprocess.Popen(["dpkg", "--get-selections"], stdout=subprocess.PIPE)
+    cmd_result = subprocess.check_output(["grep", "browser"], stdin=ps.stdout)
+    ps.wait()
+
+    browser = cmd_result.decode().split("\t")[0]
+
+    webbrowser.get(browser).open(url)
+
 try:
     # Open the web browser to the Home Assistant page where to configure the MQTT integration
     url = "http://127.0.0.1:8123/_my_redirect/config_flow_start?domain=mqtt"
-    webbrowser.open(url)
+    open_web_page(url)
 
     # Get the MQTT broker info from the catalog
     url = "http://127.0.0.1:8099/getInfo"
@@ -35,6 +45,6 @@ try:
 
     # Open the web browser to the Home Assistant page where to configure the localIP integration
     url = "http://127.0.0.1:8123/_my_redirect/config_flow_start?domain=local_ip"
-    webbrowser.open(url)
+    open_web_page(url)
 except Exception as e:
     print(str(e))
